55a0577f589978eb8049953fb9a11757bacdc0c3
Elena <lenaromasenco@gmail.com>
2024-09-10 12:55:19 +0300

    Add GetUsersListQuery and GetNotPaginatedUsersListQuery

---
+++ NFT.UseCases/User/Queries/GetNotPaginatedUsersListQuery.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NFT.Shared.DataTransferObjects.Users;
+using NFT.Infrastructure;
+
+namespace SFRAdmin.UseCases.Users.Queries
+{
+    public class GetNotPaginatedUsersListQuery : IRequest<List<UserDto>>
+    {
+        public GetNotPaginatedUsersListQuery()
+        {
+        }
+    }
+
+    public class GetNotPaginatedUsersListQueryHandler : IRequestHandler<GetNotPaginatedUsersListQuery, List<UserDto>>
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public GetNotPaginatedUsersListQueryHandler(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<UserDto>> Handle(GetNotPaginatedUsersListQuery request, CancellationToken cancellationToken)
+        {
+            return await _appDbContext.Users.Select(user => new UserDto
+            {
+                Id = user.Id,
+                Login = user.Login
+            }).ToListAsync(cancellationToken);
+        }
+    }
+}
--- NFT/Client/Program.cs
+++ NFT/Client/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using NFT.Client;
-
 using NFT.Client.Services.UserServices;
 
 
