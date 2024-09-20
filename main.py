97fb17c23777bf5144f6bba4927f1eca5fc2a1e6
scott1q <cvasiuc.dima@gmail.com>
2024-09-20 19:14:19 +0300

    added the model which uses logistic regression

---
+++ main.py
@@ -0,0 +1,18 @@
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'[\n\t]', ' ', text)
+    text = ' '.join([word for word in text.split() if word not in stop_words])
+    text = ' '.join([lemmatizer.lemmatize(word) for word in text.split()])
+    return text
