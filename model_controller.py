38a7a9870925a3e6cedcd9cf5be7831eb05f6b42
scott1q <cvasiuc.dima@gmail.com>
2024-10-09 19:52:00 +0300

    Added the controller for the ML model

---
+++ model_controller.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import joblib
+from main import clean_text
+
+app = Flask(__name__)
+
+with open('moderation_model.pkl', 'rb') as file:
+    model = joblib.load(file)
+
+with open('vectorizer.pkl', 'rb') as file:
+    vectorizer = joblib.load(file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    texts_data = data.get('texts', [])
+    if not texts_data:
+        return jsonify({'error': 'No texts provided'}), 400
+    
+    texts_cleaned = [clean_text(text) for text in texts_data]
+    texts_vectorized = vectorizer.transform(texts_cleaned)
+
+    prediction = model.predict(texts_vectorized)
+    
+    return jsonify({'Prediction made': prediction.tolist()})
+
+app.run(debug=True)
