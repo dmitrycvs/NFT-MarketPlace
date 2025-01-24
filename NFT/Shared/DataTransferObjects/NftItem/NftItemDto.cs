b414ed2857c3ab4b9f1021cba94bebbe38115027
Elena <lenaromasenco@gmail.com>
2025-01-24 18:50:53 +0200

    NftDetailsPage implemented

--- NFT/Shared/DataTransferObjects/NftItem/NftItemDto.cs
+++ NFT/Shared/DataTransferObjects/NftItem/NftItemDto.cs
@@ -5,6 +5,8 @@ public class NftItemDto
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Hash { get; set; }
+    public string? ImageBase64 { get; set; }
+
     public Guid UserId { get; set; }
     public decimal Price { get; set; }
     public bool IsListed { get; set; } = false;
